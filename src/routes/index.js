3c2d544669ad2bf30f48ac4e659dce57a4ac618f
matrawy-maxbot <114000998+matrawy-maxbot@users.noreply.github.com>
2024-03-03 00:47:17 +0200

    Delete src/routes/index.js

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import getIndex from '../controllers/index.js';
-import { call } from '../middleware/handleError.js';
-const router = express.Router();
-
-router.get('/', call(getIndex));
-
-export default router;
