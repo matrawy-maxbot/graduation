69397e8f1b60456c9b6ce85f48df4259a0879bc0
matrawy-maxbot <114000998+matrawy-maxbot@users.noreply.github.com>
2024-05-23 04:58:37 +0300

    Add files via upload

--- src/routes/appointment.js
+++ src/routes/appointment.js
@@ -4,6 +4,8 @@ import { checkRequired } from '../middleware/plugins.js';
 import { checkDoctor } from '../controllers/doctors.js';
 import { checkToken, checkRole } from '../middleware/authentication.js';
 import { call } from '../middleware/handleError.js';
+import { checkDate } from '../database/tableSettings.js';
+import { checkDateOnDoctor } from '../controllers/appointment.js';
 const router = express.Router();
 router.use(express.json());
 
@@ -16,7 +18,7 @@ router.get('/doctors', call(checkToken), call(checkRole, "admin"), call(getDocto
 router.get('/doctors/:id', call(checkToken), call(checkRole, "admin"), call(getDoctorAppointments));
 router.get('/patients', call(checkToken), call(checkRole, "admin"), call(getPatientsAppointments));
 router.get('/patients/:name', call(checkToken), call(checkRole, "admin"), call(getPatientAppointments));
-router.post('/:id', call(checkToken), call(checkRole, ["user","unsystem"]), call(checkRequired, ['name', 'phone', 'age', 'sex', 'app_date']), call(checkDoctor), call(createAppointment));
+router.post('/:id', call(checkToken), call(checkRole, ["user","unsystem"]), call(checkRequired, ['name', 'phone', 'age', 'sex', 'app_date']), call(checkDoctor), call((req, res, next) => {try{checkDate(req.body.app_date, req, res, next)} catch (err) { throw err; }}), call(checkDateOnDoctor), call(createAppointment));
 router.post('/:id/completed', call(checkToken), call(checkRole, ["doctor","unsystem"]), call(checkAppointment), call(completeAppointment));
 router.delete('/:id', call(checkToken), call(checkRole, "all"), call(checkAppointment), call(deleteAppointment));
 
